0f098635966fb2b115135a420cbe4ba92fd731b6
TannerEsch <eschmanntanner@gmail.com>
2023-03-13 15:44:10 -0400

    necessary dependencies installed, config files set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/app.css'
+
+const App = () => {
+  return (
+    <div>
+        ooooo
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx,tsx,ts}"],
+    mode: "jit",
+    theme: {
+      extend: {
+        colors: {
+          primary: "hsl(0, 0%, 18%)",
+          secondary: "#e5e7eb",
+          tertiary: "#E6E2DD",
+          four: "#F2F2F2",
+          dimWhite: "rgba(255, 255, 255, 0.7)",
+          dimBlue: "rgba(9, 151, 124, 0.1)",
+          blues: '#aeecfc',
+          transBlack: "rgba(0, 0, 0, 0.5)"
+        },
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
+    plugins: [],
+  };
