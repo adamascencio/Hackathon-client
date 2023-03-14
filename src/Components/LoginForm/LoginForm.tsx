646854253db9160bbf0e24ca4c47f584e530f44c
adamascencio <ascencioadam@gmail.com>
2023-03-14 16:09:47 -0700

    Add styles and react-icons to loginform component

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -1,6 +1,8 @@
 // npm modules
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { FaLock } from 'react-icons/fa';
 
 // services
 import styles from './LoginForm.module.css'
@@ -14,11 +16,11 @@ import { LoginFormData } from '../../types/forms'
 import { handleErrMsg } from '../../types/validators'
 
 const LoginForm = (props: AuthFormProps): JSX.Element => {
-  const {updateMessage, handleAuthEvt} = props
+  const { updateMessage, handleAuthEvt } = props
   const navigate = useNavigate()
 
   const [formData, setFormData] = useState<LoginFormData>({
-    email: '',
+    username: '',
     password: '',
   })
 
@@ -39,41 +41,55 @@ const LoginForm = (props: AuthFormProps): JSX.Element => {
     }
   }
 
-  const { email, password } = formData
+  const { username, password } = formData
 
   const isFormInvalid = (): boolean => {
-    return !(email && password)
+    return !(username && password)
   }
 
   return (
     <form
       autoComplete="off"
       onSubmit={handleSubmit}
-      className={styles.container}
+      className='flex flex-col items-center gap-10'
     >
-      <div className={styles.inputContainer}>
-        <label htmlFor="email" className={styles.label}>Email</label>
-        <input
-          type="text"
-          id="email"
-          value={formData.email}
-          name="email"
-          onChange={handleChange}
-        />
+      <div className='flex flex-col gap-2'>
+        <div className="relative inline-flex items-center">
+          <input
+            type="text"
+            id="username"
+            value={formData.username}
+            name="username"
+            onChange={handleChange}
+            className='rounded-lg px-3 py-2 opacity-50 text-black font-bold text-lg'
+          />
+          {formData.username === '' && 
+          <div className="absolute left-7 top-2 text-lpBg font-light flex items-center gap-2">
+            <BsFillPersonFill />
+            <label htmlFor='username'>Username</label>
+          </div>
+          }
+        </div>
+        <div className="relative">
+          <input
+            type="password"
+            id="password"
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+            className='rounded-lg px-3 py-2 opacity-50 text-black font-bold text-lg'
+          />
+          {formData.password === '' && 
+          <div className="absolute left-7 top-2 text-lpBg font-light flex items-center gap-2">
+            <FaLock />
+            <label htmlFor='password'>Password</label>
+          </div>
+          }
+        </div>
       </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="password" className={styles.label}>Password</label>
-        <input
-          type="password"
-          id="password"
-          value={formData.password}
-          name="password"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <button disabled={isFormInvalid()} className={styles.button}>
-          Log In
+      <div className='grid gap-4 w-5/6 max-w-xs'>
+        <button disabled={isFormInvalid()} className='rounded-full bg-gradient-to-r from-loginBtnDark to-loginBtnLight py-2 border-2 border-btnBorder outline outline-btnBorder outline-offset-3 shadow-md'>
+          LOGIN
         </button>
         <Link to="/">
           <button>Cancel</button>
