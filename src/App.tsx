e3a73a7682e6763a264174ac4e29acaa291e67b7
Maripirs <mariapazmaluenda@gmail.com>
2023-03-14 19:31:49 -0700

    added the rocket level

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,82 @@
-// npm modules 
+// npm modules
 import React, { useEffect, createContext, useState } from "react";
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 // page components
-import Signup from './pages/Signup/Signup'
-import Login from './pages/Login/Login'
-import Landing from './pages/Landing/Landing'
-import Profiles from './pages/Profiles/Profiles'
-import ChangePassword from './pages/ChangePassword/ChangePassword'
-import ProfilePage from './pages/Profile/Profile'
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Landing from "./pages/Landing/Landing";
+import Profiles from "./pages/Profiles/Profiles";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import ProfilePage from "./pages/Profile/Profile";
+import Level1 from "./pages/Levels/Level1";
 
 // components
-import NavBar from './Components/NavBar/NavBar'
-import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
+import NavBar from "./Components/NavBar/NavBar";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 // services
-import * as authService from './Services/authService'
+import * as authService from "./Services/authService";
 
 // stylesheets
-import './css/app.css'
+import "./css/app.css";
 
 //utils
-import calculate from './utils/math'
+import calculate from "./utils/math";
 // types
-import { Profile, User } from './types/models'
+import { Profile, User } from "./types/models";
 
 function App(): JSX.Element {
-  const navigate = useNavigate()
-  
-  const [user, setUser] = useState<User | null>(authService.getUser())
+    const navigate = useNavigate();
 
-  const handleLogout = (): void => {
-    authService.logout()
-    setUser(null)
-    navigate('/')
-  }
+    const [user, setUser] = useState<User | null>(authService.getUser());
 
-  const handleAuthEvt = (): void => {
-    setUser(authService.getUser())
-  }
-  
-  console.log(calculate(5, 10))
+    const handleLogout = (): void => {
+        authService.logout();
+        setUser(null);
+        navigate("/");
+    };
 
-  return (
-    <>
-      {user && <NavBar user={user} handleLogout={handleLogout} />}
-      <Routes>
-        <Route path="/" element={<Landing user={user} />} />
-        <Route path="/test" element={<TestComponent/>} />
-        <Route path="/signup" element={<Signup handleAuthEvt={handleAuthEvt} />}/>
-        <Route path="/login" element={<Login handleAuthEvt={handleAuthEvt} />}/>
-        <Route path="/profiles" element={<ProtectedRoute user={user}><Profiles /></ProtectedRoute>}/>
-        <Route path="/change-password" element={ <ProtectedRoute user={user}><ChangePassword handleAuthEvt={handleAuthEvt} /></ProtectedRoute>}/>
-      </Routes>
-    </>
-  )
+    const handleAuthEvt = (): void => {
+        setUser(authService.getUser());
+    };
+
+    console.log(calculate(5, 10));
+
+    return (
+        <>
+            {user && <NavBar user={user} handleLogout={handleLogout} />}
+            <Routes>
+                <Route path="/" element={<Landing user={user} />} />
+                {/* <Route path="/test" element={<TestComponent />} /> */}
+                <Route
+                    path="/signup"
+                    element={<Signup handleAuthEvt={handleAuthEvt} />}
+                />
+                <Route
+                    path="/login"
+                    element={<Login handleAuthEvt={handleAuthEvt} />}
+                />
+                <Route
+                    path="/profiles"
+                    element={
+                        <ProtectedRoute user={user}>
+                            <Profiles />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/change-password"
+                    element={
+                        <ProtectedRoute user={user}>
+                            <ChangePassword handleAuthEvt={handleAuthEvt} />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/level1" element={<Level1 />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,37 +3,45 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{js,jsx,tsx,ts}"],
     mode: "jit",
     theme: {
-      extend: {
-        colors: {
-          primary: "hsl(0, 0%, 18%)",
-          secondary: "#e5e7eb",
-          tertiary: "#E6E2DD",
-          four: "#F2F2F2",
-          dimWhite: "rgba(255, 255, 255, 0.7)",
-          dimBlue: "rgba(9, 151, 124, 0.1)",
-          blues: '#aeecfc',
-          transBlack: "rgba(0, 0, 0, 0.5)",
-          lpBg: "rgb(80, 100, 186)",
-          btnBorder: "rgb(60, 73, 126)",
-          loginBtnDark: "rgb(46, 56, 80)",
-          loginBtnLight: "rgb(67, 80, 142)"
+        extend: {
+            colors: {
+                primary: "hsl(0, 0%, 18%)",
+                secondary: "#e5e7eb",
+                tertiary: "#E6E2DD",
+                four: "#F2F2F2",
+                dimWhite: "rgba(255, 255, 255, 0.7)",
+                dimBlue: "rgba(9, 151, 124, 0.1)",
+                blues: "#aeecfc",
+                transBlack: "rgba(0, 0, 0, 0.5)",
+                lpBg: "rgb(80, 100, 186)",
+                btnBorder: "rgb(60, 73, 126)",
+                loginBtnDark: "rgb(46, 56, 80)",
+                loginBtnLight: "rgb(67, 80, 142)",
+                completeTop: "0D6FCA",
+                completeBottom: "4B0384",
+            },
+            fontFamily: {
+                nunito: ["Nunito", "sans-serif"],
+            },
+            translate: {
+                xInput: "translate(12px) scale(1)",
+                yInput: "translate(16px) scale(1)",
+            },
         },
-        fontFamily: {
-          nunito: ['Nunito', "sans-serif"]
+        screens: {
+            xs: "480px",
+            ss: "620px",
+            sm: "768px",
+            md: "1060px",
+            lg: "1200px",
+            xl: "1700px",
         },
-        translate: {
-          xInput: "translate(12px) scale(1)",
-          yInput: "translate(16px) scale(1)"
+        fontWeight: {
+            thin: "300",
+            boldish: "400",
+            bold: "600",
+            bolder: "700",
         },
-      },
-      screens: {
-        xs: "480px",
-        ss: "620px",
-        sm: "768px",
-        md: "1060px",
-        lg: "1200px",
-        xl: "1700px",
-      },
     },
     plugins: [],
-  };
+};
