fa4d7dab492e13b92f90f27217f27262d12e0fae
TannerEsch <eschmanntanner@gmail.com>
2023-03-14 21:10:08 -0400

    fixed navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -19,24 +19,30 @@ interface NavBarProps {
 const NavBar = (props: NavBarProps): JSX.Element => {
   const { user, handleLogout } = props
   
-  return (
+  return <>
+    
     <div className="flex items-center justify-center">
-      {window.location.pathname =="/login" ||window.location.pathname =="/signup"? null :
+      
   <div className='fixed flex bottom-0 gap-16 p-4 text-3xl border-gray-400 bg-gray-300 border-[1px] mb-8 rounded-[100px] shadow-2xl'>
+    {user?
             <>
           <NavLink to="/"><AiFillHome/></NavLink>
           <NavLink to="/profiles"><CgMathDivide/></NavLink>
           <NavLink to="/profiles"><TbSwords/></NavLink>
           <NavLink to="/profile"><BsFillPersonFill/></NavLink>
           </>
-          <>
+          :
+          <> 
           <NavLink to="/login">Log In</NavLink>
           <NavLink to="/signup">Sign Up</NavLink>
-          </>  
+          </>   
+          }
       </div>
-      }
+     
       </div>
-  )
+      </>
+    
+  
 }
 
 export default NavBar
