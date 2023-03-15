137cd5069244c09a80056d4c624f46c4c4c01769
TannerEsch <eschmanntanner@gmail.com>
2023-03-15 13:17:10 -0400

    just saving for a pull

--- src/pages/Worlds.tsx
+++ src/pages/Worlds.tsx
@@ -19,7 +19,7 @@ const Worlds = (props: WorldsProps): JSX.Element => {
     <div onClick={(e) =>console.log('hi')}  className='bg-transparent w-4 h-4 z-10 top-[27rem] scale-[7.2] left-[17.3rem] rounded-xl absolute'></div>
     <div onClick={(e) =>console.log('hi')}  className='bg-transparent w-4 h-4 z-10 top-[33rem] scale-[5] left-[9.3rem] rounded-xl absolute'></div>
     <div onClick={(e) =>console.log('hi')}  className='bg-transparent w-4 h-4 z-10 top-[42rem] left-[4.5rem] scale-[5] rounded-xl absolute'></div>
-    <div onClick={(e) =>console.log('hi')}  className='w-screen h-screen flex overflow-clip' id="world">
+    <div className='w-screen h-screen flex overflow-clip' id="world">
       <div className='flex fixed w-screen justify-between p-5 top-1'>
       <div className="bg-gray-800 flex gap-1 rounded-xl w-16 p-1">
       <img src={star}  />
