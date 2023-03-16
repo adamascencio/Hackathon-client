a4e995fc688f516acea3fbef0c20e9b99de45985
TannerEsch <eschmanntanner@gmail.com>
2023-03-15 20:25:45 -0400

    added a .gitignore

--- client/src/pages/Worlds.tsx
+++ client/src/pages/Worlds.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import solarsystem from './solarsystem.png'
 import star from './Star.png'
 import moon from './Moon.png'
-import './world.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
@@ -10,35 +9,31 @@ import { useState } from 'react'
 import * as progressService from '../Services/progressService'
 
 import { User, Profile, Progress } from './../types/models'
-interface WorldsProps {
-  user: User | null;
-  profile: Profile
-}
 
 const Worlds = (props: any): JSX.Element => {
   const { user, profile } = props
   const navigate = useNavigate()
 
-  const [progress, setProgress] = useState<Progress>({
-    profile: profile.id,
-    //! need to be able to add level and world id to this form
-    level: 0,
-    world: 0,
-    levelCompleted: false,
-    //! i'm not sure what is supposed to be in exercisesComplete
-    // exercisesComplete: "",
-  })
+  // const [progress, setProgress] = useState<Progress>({
+  //   profile: profile.id,
+  //   //! need to be able to add level and world id to this form
+  //   level: 0,
+  //   world: 0,
+  //   levelCompleted: false,
+  //   //! i'm not sure what is supposed to be in exercisesComplete
+  //   // exercisesComplete: "",
+  // })
 
   //! once start button is implemented we'll set an onClick for that element to trigger the below function
-  const handleLevelStart = async (evt: React.FormEvent): Promise<void> => {
-    evt.preventDefault()
-    try {
-      await progressService.createProgress(progress)
-      navigate('/level1')
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // const handleLevelStart = async (evt: React.FormEvent): Promise<void> => {
+  //   evt.preventDefault()
+  //   try {
+  //     await progressService.createProgress(progress)
+  //     navigate('/level1')
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
 
   return (
@@ -49,7 +44,7 @@ const Worlds = (props: any): JSX.Element => {
       <div onClick={(e) => console.log('hi')} className='bg-transparent w-4 h-4 z-10 top-[27rem] scale-[7.2] left-[17.3rem] rounded-xl absolute'></div>
       <div onClick={(e) => console.log('hi')} className='bg-transparent w-4 h-4 z-10 top-[33rem] scale-[5] left-[9.3rem] rounded-xl absolute'></div>
       <div onClick={(e) => console.log('hi')} className='bg-transparent w-4 h-4 z-10 top-[42rem] left-[4.5rem] scale-[5] rounded-xl absolute'></div>
-      <div onClick={(e) => console.log('hi')} className='w-screen h-screen flex overflow-clip' id="world">
+      <div onClick={(e) => console.log('hi')} className='w-screen h-screen flex overflow-clip'>
         <div className='flex fixed w-screen justify-between p-5 top-1'>
           <div className="bg-gray-800 flex gap-1 rounded-xl w-16 p-1">
             <img src={star} />
