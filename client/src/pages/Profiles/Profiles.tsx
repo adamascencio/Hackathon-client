678fe316ce26fb48d184d09d5715136b5a92f77e
adamascencio <ascencioadam@gmail.com>
2023-03-15 20:11:57 -0700

    Pin Daily Challenge text inside container so text remains locked on screen resize. Align items center on desktop.

--- client/src/pages/Profiles/Profiles.tsx
+++ client/src/pages/Profiles/Profiles.tsx
@@ -26,18 +26,18 @@ const Profiles = (): JSX.Element => {
   }, [])
 
   return (
-    <div className='bg-orbit'>     
-     <div className="flex">
-      <div className='text-[35px] mt-10 ml-2 font-nunito'>Welcome back!</div>
-      <img src="" alt="" className='rounded-[50%] border-black border-[1px] w-20 h-20 ml-auto m-4'></img>
-    </div>
+    <div className='bg-orbit px-5'>     
+      <div className='flex items-center'>
+        <div className='text-[35px] font-nunito'>Welcome back!</div>
+        <img src="" alt="" className='rounded-[50%] border-black border-[1px] w-20 h-20 ml-auto m-4'></img>
+      </div>
 
-    <div className="flex flex-col gap-4">
-    <ProfileDailyChallenge/>
-    <ProfileFeaturedGames/>
-    <div className='w-screen overflow-x-scroll'>
-    <ProfileMathByTopic/>
-    </div>
+    <div className="flex flex-col gap-4 xs:items-center">
+      <ProfileDailyChallenge/>
+      <ProfileFeaturedGames/>
+      <div className='flex overflow-x-scroll xs:items-center'>
+        <ProfileMathByTopic/>
+      </div>
     </div>
 
     </div>
